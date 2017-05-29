3f0bba66aa1bf014cd77d9e409033c91aa7d484f
skgrange <skgrange@gmail.com>
2017-05-29 16:30:20 +0100

    update help for str_wkt

--- R/str_wkt.R
+++ R/str_wkt.R
@@ -1,24 +1,45 @@
 #' Function to create WKT strings. 
 #' 
-#' @param latitude
+#' @param latitude Latitude vector. 
 #' 
-#' @param longitude
+#' @param longitude Longitude vector. 
 #' 
-#' @param type
+#' @param type Type of geom to create. Only \code{"points"} are supported 
+#' currently. 
+#' 
+#' @param round Decimal points to round \code{latitude} and \code{longitude}
+#' vector. 
 #' 
 #' @author Stuart K. Grange
 #' 
 #' @return Character vector.
 #' 
 #' @export
-str_wkt <- function(latitude, longitude, type = "point") {
+str_wkt <- function(latitude, longitude, type = "point", round = NA) {
   
-  # Parse
+  # Parse argument
   type <- stringr::str_trim(type)
   type <- stringr::str_to_lower(type)
   
-  if (type %in% c("point", "points")) 
+  # Round variables
+  if (!is.na(round)) {
+    
+    latitude <- round(latitude, round)
+    longitude <- round(longitude, round)
+    
+  }
+  
+  # Build the strings
+  if (type %in% c("point", "points")) {
+    
     geom <- stringr::str_c("POINT (", longitude, " ", latitude, ")")
+    
+  } else {
+    
+    warning("'type' not supported...", call. = FALSE)
+    geom <- NA
+    
+  }
   
   return(geom)
   
