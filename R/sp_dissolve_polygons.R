844987a29875730a4babc870e49109986b3b1c57
skgrange <skgrange@gmail.com>
2017-12-20 16:26:20 +0000

    add sp_make_grid

--- R/sp_dissolve_polygons.R
+++ R/sp_dissolve_polygons.R
@@ -3,9 +3,15 @@
 #' 
 #' If the individual polygons have data associated with them, it will be 
 #' dropped when the polygons are united. 
+#' 
+#' @param sp Spatial polygons. 
+#' 
+#' @param features Should features within \code{sp} attempted to be preserved? 
 #'
 #' @author Stuart K. Grange
+#' 
+#' @return Spatial polygons. 
 #'
 #' @export
-sp_dissolve_polygons <- function(sp, feature = NULL) 
-  rgeos::gUnaryUnion(sp, id = feature)
+sp_dissolve_polygons <- function(sp, features = NULL) 
+  rgeos::gUnaryUnion(sp, id = features)
---
+++ R/sp_make_grid.R
@@ -0,0 +1,29 @@
+#' Function to make a gridded spatial points object. 
+#' 
+#' @author Stuart K. Grange
+#' 
+#' @param sp Spatial polygons. 
+#' 
+#' @param cellsize Resolution of spatial grid in the projection system of 
+#' \code{sp}. 
+#' 
+#' @return Spatial points. 
+#'
+#' @export
+sp_make_grid <- function(sp, cellsize) {
+  
+  # Save projection string
+  projection <- sp_projection(sp)
+  
+  # Make the grid
+  df <- sp::makegrid(sp, cellsize = cellsize) 
+  
+  # Promote
+  sp <- sp::SpatialPoints(df)
+  
+  # Give projection
+  if (!is.na(projection)) sp <- sp_transform(sp, projection, warn = FALSE)
+  
+  return(sp)
+  
+}
