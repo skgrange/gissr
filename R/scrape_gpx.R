90e00acbdf9dba599db0cd678e67ddf1f644a133
skgrange <skgrange@gmail.com>
2018-05-15 11:22:40 +0100

    enhance scrape_gpx

--- R/scrape_gpx.R
+++ R/scrape_gpx.R
@@ -4,7 +4,7 @@
 #' @param file GPX file. 
 #' 
 #' @param transform Should latitude and longitude be used to calculate speed and
-#' distance too? 
+#' distance if the file contains the appropriate variables? 
 #' 
 #' @author Stuart K. Grange
 #' 
@@ -15,19 +15,6 @@ scrape_gpx <- function(file, transform = TRUE) {
   
   # Load file as text
   text_gpx <- threadr::read_lines(file, warn = FALSE)
-  
-  # Get a few attribures
-  # attributes <- threadr::str_filter(head(text_gpx), "xmlns")
-  # 
-  # values <- stringr::str_extract_all(attributes, '"[^"]*"')[[1]]
-  # values <- stringr::str_replace_all(values, "\"", "")
-  # 
-  # variables <- stringr::str_split(attributes, "=")[[1]]
-  # variables <- variables[-length(variables)]
-  # variables <- stringr::str_replace(variables, ".* ", "")
-  # 
-  # # Give names
-  # list_attributes <- setNames(as.list(values), variables)
   
   # Parse xml, to-do, find out why a html parser is needed. 
   xml_tree <- XML::htmlTreeParse(text_gpx, useInternalNodes = TRUE)
@@ -45,28 +32,40 @@ scrape_gpx <- function(file, transform = TRUE) {
   elevation <- as.numeric(elevation)
   latitude <- as.numeric(latitude)
   longitude <- as.numeric(longitude)
-  date <- lubridate::ymd_hms(date,tz = "UTC")
+  date <- lubridate::ymd_hms(date, tz = "UTC")
   
   # Build data frame
-  df <- data.frame(
-    date, 
-    elevation,
-    latitude,
-    longitude,
-    stringsAsFactors = FALSE
-  )
-  
-  # Calculate things
-  if (transform) {
+  if (length(date) != 0) {
     
-    df$distance <- distance_by_haversine(df$latitude, df$longitude)
-    df$speed <- df$distance * (df$date - dplyr::lag(df$date))
-    df$speed <- as.numeric(df$speed)
-    df$speed_km_h <- threadr::ms_to_km_h(df$speed)
+    df <- data.frame(
+      date, 
+      elevation,
+      latitude,
+      longitude,
+      stringsAsFactors = FALSE
+    )
+    
+    # Calculate things, needs date
+    if (transform) {
+      
+      df$distance <- distance_by_haversine(df$latitude, df$longitude)
+      df$speed <- df$distance * (df$date - dplyr::lag(df$date))
+      df$speed <- as.numeric(df$speed)
+      df$speed_km_h <- threadr::ms_to_km_h(df$speed)
+      
+    }
+    
+  } else {
+    
+    # When the gpx file does not contain date
+    df <- data.frame(
+      elevation,
+      latitude,
+      longitude,
+      stringsAsFactors = FALSE
+    )
     
   }
-  
-  # attributes(df)$gpx <- list_attributes
   
   return(df)
   
