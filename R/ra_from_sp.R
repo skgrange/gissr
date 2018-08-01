8826e98bf1fb80ebb18f58c238bea30c17f13f0a
skgrange <skgrange@gmail.com>
2018-08-01 14:58:16 +0100

    :sparkles: enhance ra_from_sp to accept additional arguments & fixes #5

--- R/ra_from_sp.R
+++ R/ra_from_sp.R
@@ -4,22 +4,36 @@
 #' 
 #' @param resolution Resolution of the returned raster object.  
 #' 
+#' @param fun Function to determine how multiple observations in a single raster 
+#' cell are handled. 
+#' 
+#' @param na.rm For \code{fun}, should \code{NA}s be ignored? 
+#' 
+#' @param background Cell's default value when there are no data within the cell. 
+#' 
 #' @param drop_index Should the index be dropped from the raster return? 
 #' 
 #' @author Stuart K. Grange
 #' 
-#' @return Raster layer or brick. 
+#' @return Raster layer or raster brick. 
 #' 
 #' @seealso \code{\link{rasterize}} 
 #' 
 #' @export
-ra_from_sp <- function(sp, resolution, drop_index = TRUE) {
+ra_from_sp <- function(sp, resolution, fun = mean, na.rm = TRUE,
+                       background = NA, drop_index = TRUE) {
   
   # Create blank raster
   ra_blank <- ra_create(sp, resolution)
   
   # Rasterise sp
-  ra <- raster::rasterize(sp, ra_blank)
+  ra <- raster::rasterize(
+    x = sp, 
+    y = ra_blank, 
+    fun = fun, 
+    background = background,
+    na.rm = na.rm
+  )
   
   # Drop the index
   if (drop_index) ra <- ra[[-1]]
