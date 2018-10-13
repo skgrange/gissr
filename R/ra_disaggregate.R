8d8e9bc3d674065deb396f25226e7f8930bebfea
skgrange <s.k.grange@gmail.com>
2018-10-13 08:14:55 +0100

    :sparkles: add ra_disaggregate

---
+++ R/ra_disaggregate.R
@@ -0,0 +1,22 @@
+#' Function to create a higher resolution ("disaggregate") raster layer. 
+#' 
+#' @author Stuart K. Grange
+#' 
+#' @param ra Raster object. 
+#' 
+#' @param factor Amount of disaggregation in number of cells. 
+#' 
+#' @param method Method to use for interpolation. 
+#' 
+#' @return Raster object. 
+#' 
+#' @examples 
+#' \dontrun{
+#' 
+#' ra_countries_smooth <- ra_disaggregate(ra_countries, factor = 5)
+#' 
+#' }
+#' 
+#' @export
+ra_disaggregate <- function(ra, factor, method = "bilinear")
+  raster::disaggregate(ra, fact = factor, method = method)
