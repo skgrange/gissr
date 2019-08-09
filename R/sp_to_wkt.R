dd977311f9a1afe0421310a0af06d122450cd076
skgrange <s.k.grange@gmail.com>
2019-08-09 14:59:29 +0200

    :sparkles: sp_to_wkt will now return a tibble

--- R/sp_to_wkt.R
+++ R/sp_to_wkt.R
@@ -1,18 +1,19 @@
-#' Function to convert a spatial object to a vector of WKT or a data frame 
+#' Function to convert a spatial object to a vector of WKT or a tibble 
 #' containing a WKT variable. 
 #' 
 #' @param sp Spatial object to convert to WKT. 
 #' 
 #' @param features Should features within \code{sp} form different WKT strings?
-#' Default is \code{TRUE}. 
 #' 
 #' @param data If \code{sp} contains a data slot, should a data frame be created
-#' with a WKT variable? Default is \code{TRUE}. 
+#' with a WKT variable? 
 #' 
 #' @param wkt Variable name of WKT strings in the returned data frame is 
-#' \code{data} is \code{TRUE}. Default is \code{"geom"}.
+#' \code{data} is \code{TRUE}.
 #' 
 #' @author Stuart K. Grange
+#' 
+#' @return Tibble. 
 #'
 #' @export
 sp_to_wkt <- function(sp, features = TRUE, data = TRUE, wkt = "geom") {
@@ -29,6 +30,9 @@ sp_to_wkt <- function(sp, features = TRUE, data = TRUE, wkt = "geom") {
     # Add to df as a variable
     df[, wkt] <- text
     
+    # To tibble
+    df <- tibble::as_tibble(df)
+    
   } else {
     
     # No data slot, just create wkt
@@ -37,7 +41,6 @@ sp_to_wkt <- function(sp, features = TRUE, data = TRUE, wkt = "geom") {
     
   }
   
-  # Return
-  df
+  return(df)
   
 }
