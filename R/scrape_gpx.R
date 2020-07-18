f8c1425d637f73ba4a8b95719a95e8f7514fcd11
skgrange <s.k.grange@gmail.com>
2020-07-18 10:46:07 +0200

    :sparkles: significantly improve scrape_gpx and include new tests

--- R/scrape_gpx.R
+++ R/scrape_gpx.R
@@ -1,55 +1,111 @@
-#' Function to read \code{GPX} files as a data frame from the GPX Logger Android
-#' phone app. 
+#' Function to read \code{GPX} files as tabular data.  
 #' 
-#' @param file GPX file. 
+#' @param file A vector of GPS file names. 
 #' 
 #' @param transform Should latitude and longitude be used to calculate speed and
 #' distance if the file contains the appropriate variables? 
+#' 
+#' @param .id Optional variable name for file name in the returned tibble.  
+#' 
+#' @param verbose Should the function give messages? 
 #' 
 #' @author Stuart K. Grange
 #' 
 #' @return Tibble. 
 #' 
 #' @export
-scrape_gpx <- function(file, transform = TRUE) {
+scrape_gpx <- function(file, transform = TRUE, .id = NULL, verbose = FALSE) {
+  
+  file %>% 
+    purrr::set_names(.) %>% 
+    purrr::map_dfr(
+      scrape_gpx_worker, transform = transform, verbose = verbose, .id = .id
+    )
+  
+}
+
+
+scrape_gpx_worker <- function(file, transform, verbose) {
+  
+  # Message to user
+  if (verbose) message(threadr::date_message(), "`", file, "`...")
   
   # Load file as text
   text_gpx <- readr::read_lines(file)
   
-  # Parse xml, to-do, find out why a html parser is needed. 
+  # Parse xml, to-do, find out why a html parser is needed
   xml_tree <- XML::htmlTreeParse(text_gpx, useInternalNodes = TRUE)
+  
+  # Extended file version? 
+  # For garmin watches
+  gpx_extended <- any(stringr::str_detect(text_gpx[1:10], "TrackPointExtension"))
   
   # Get variables
   coordinates <- XML::xpathSApply(xml_tree, path = "//trkpt", XML::xmlAttrs)
-  elevation <- XML::xpathSApply(xml_tree, path = "//trkpt/ele", XML::xmlValue)
-  date <- XML::xpathSApply(xml_tree, path = "//trkpt/time", XML::xmlValue)
   
-  # Ge latitude and longitude
-  latitude <- coordinates["lat", ]
-  longitude <- coordinates["lon", ]
+  # Stop here
+  if (length(coordinates) == 0L) {
+    stop(
+      "GPX file does not contain coordinates in a standard location.", 
+      call. = FALSE
+    )
+  }
   
-  # Data types
-  elevation <- as.numeric(elevation)
-  latitude <- as.numeric(latitude)
-  longitude <- as.numeric(longitude)
-  date <- lubridate::ymd_hms(date, tz = "UTC")
+  # Get latitude and longitude
+  latitude <- coordinates["lat", ] %>% 
+    as.numeric()
   
+  longitude <- coordinates["lon", ] %>% 
+    as.numeric()
+  
+  elevation <- xml_tree %>% 
+    XML::xpathSApply(path = "//trkpt/ele", XML::xmlValue) %>% 
+    as.numeric()
+  
+  # When there is no elevation data
+  if (length(elevation) == 0L) elevation <- NA_real_
+  
+  date <- xml_tree %>% 
+    XML::xpathSApply(path = "//trkpt/time", XML::xmlValue) %>% 
+    lubridate::ymd_hms(tz = "UTC")
+
+  # Extended variables for garmin watches
+  if (gpx_extended) {
+    
+    heart_rate <- xml_tree %>% 
+      XML::xpathSApply(path = "//hr", XML::xmlValue) %>%
+      as.numeric()
+    
+    cadence <- xml_tree %>% 
+      XML::xpathSApply(path = "//cad", XML::xmlValue) %>% 
+      as.numeric()
+    
+  }
+
   if (length(date) != 0) {
     
-    # Build tibble    
+    # Build tibble
     df <- tibble(date, elevation, latitude, longitude)
+    
+    # Add extensions too
+    if (gpx_extended) {
+      df <- mutate(df, heart_rate = !!heart_rate, cadence = !!cadence)
+    }
     
     # Calculate things, needs date
     if (transform) {
       df <- df %>% 
-        mutate(distance = distance_by_haversine(latitude, longitude),
-               speed = distance * (date - dplyr::lag(date)),
-               speed = as.numeric(speed),
-               speed_km_h = threadr::ms_to_km_h(speed))
+        mutate(time_elapsed = date - min(date),
+               time_elapsed = hms::as_hms(time_elapsed),
+               distance = distance_by_haversine(latitude, longitude),
+               time_lag = threadr::lag_delta(as.numeric(date)),
+               speed = distance / time_lag,
+               speed_km_h = threadr::ms_to_km_h(speed)) %>% 
+        select(-time_lag)
     }
     
   } else {
-    # For when the gpx file does not contain date
+    # For when the gpx file does not contain a date variable
     df <- tibble(elevation, latitude, longitude)
   }
   
--- tests/testthat/test_scrapers.R
+++ tests/testthat/test_scrapers.R
@@ -1,23 +1,18 @@
-# context("Testing scrapers")
-# 
-# test_that("Test `sp_from_js_array`", {
-#   
-#   url <- "http://skgrange.github.io/www/maps/running_tracks/data/running_tracks_north_england_polylines.js"
-#   
-#   text <- readLines(url, warn = FALSE)
-#   
-#   # No object
-#   text <- stringr::str_split_fixed(text[1], "=", 2)[, 2]
-#   text <- stringr::str_replace(text, ",];", "]")
-#   
-#   # Scrape
-#   sp_points <- sp_from_js_array(text)
-#   # sp_lines <- sp_from_js_array(text, type = "lines")
-#   sp_polygons <- sp_from_js_array(text, type = "polygons")
-#   
-#   # Test the types
-#   expect_equal(sp_class(sp_points), "SpatialPoints")
-#   # expect_equal(sp_class(sp_lines), "SpatialLines")
-#   expect_equal(sp_class(sp_polygons), "SpatialPolygons")
-# 
-# })
+context("Testing scrapers")
+
+test_that("Test `scrape_gpx`", {
+  
+  # Get all gpx files included in the package
+  file_list <- list.files(
+    system.file("extdata", package = "gissr"), 
+    pattern = "york.gpx$|lock_ness.gpx$",
+    full.names = TRUE
+  )
+  
+  # Use function
+  df <- scrape_gpx(file_list, .id = "file")
+  
+  # Check return
+  expect_equal(class(df), c("tbl_df", "tbl", "data.frame"))
+  
+})
