e755eabfd8791f70d4f66aecc22a731b1f1abee8
skgrange <skgrange@gmail.com>
2021-06-05 11:42:38 +0200

    :sparkles: replace sp_intersect & drop sp_tessellation_polygons

--- R/sp_intersect.R
+++ R/sp_intersect.R
@@ -1,14 +1,46 @@
-#' Function to intersect/clip/crop/filter spatial objects by a spatial polygon. 
+#' Function to test weather one spatial object intersects with another. 
 #' 
-#' @param sp Spatial object to be clipped. 
+#' @param sp_one First spatial object to be tested. 
 #' 
-#' @param sp_polygon Spatial polygon used as the filter. 
+#' @param sp_two Second spatial object to be tested.  
+#' 
+#' @param features Should the calculation be calculated on every feature of 
+#' \code{sp_one} and \code{sp_two}. 
+#' 
+#' @param simplify Should the return be simplified to a logical vector? 
+#' 
+#' @param check Should the intersection function perform some checks? 
 #' 
 #' @author Stuart K. Grange
 #' 
-#' @return Spatial data class, the same as \code{sp}.
+#' @return Logical matrix or vector. 
 #' 
-#' @seealso \code{\link{sp_filter}}, \code{\link{sp_clip}}
+#' @seealso \code{\link{sp_filter}}, \code{\link{sp_crop}}
 #' 
 #' @export
-sp_intersect <- function(sp, sp_polygon) raster::intersect(sp, sp_polygon)
+sp_intersect <- function(sp_one, sp_two, features = TRUE, simplify = FALSE, 
+                         check = FALSE) {
+  
+  # Check projections
+  stopifnot(identical(sp_projection(sp_one), sp_projection(sp_two)))
+  
+  # Do the calculation, returns a matrix
+  suppressWarnings(
+    x <- rgeos::gIntersects(
+      sp_one, 
+      sp_two, 
+      byid = features, 
+      prepared = TRUE, 
+      returnDense = TRUE, 
+      checkValidity = check
+    )
+  )
+  
+  # Make a logical vector if desired
+  if (simplify) {
+    x <- unname(apply(x, 1, any))
+  }
+  
+  return(x)
+  
+}
