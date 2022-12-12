03339fc2b33485673c60b18cdebdc6980cb3d543
Stuart Grange <s.k.grange@gmail.com>
2022-12-12 21:22:58 +0100

    :sparkles: add ra_summarise

---
+++ R/ra_summarise.R
@@ -0,0 +1,51 @@
+#' Function to summarise each raster layer and return results in a tibble. 
+#' 
+#' @author Stuart K. Grange
+#' 
+#' @param ra Raster object.
+#' 
+#' @param f Summary function. \code{mean} is the default. 
+#' 
+#' @param na.rm Should \code{NA}s be omitted? 
+#' 
+#' @param by_layer Should each layer be summarised? 
+#' 
+#' @param variable_as_date Are the variables/names of the \code{\link{ra_drill}}
+#' output dates? If so, the dates will be parsed and the variable renamed. 
+#' 
+#' @param tz If \code{variable_as_date} is \code{TRUE}, what time zone are the 
+#' dates stored in? This will almost certainly be \code{UTC}.
+#' 
+#' @return Tibble. 
+#' 
+#' @export
+ra_summarise <- function(ra, f = mean, na.rm = TRUE, by_layer = TRUE,
+                         variable_as_date = FALSE, tz = "UTC") {
+  
+  # Calculate the summary for each layer
+  if (by_layer) {
+    
+    # Calculate the summaries, this is a named vector
+    x <- raster::cellStats(ra, stat = f, na.rm = na.rm)
+    
+    # Make data frame
+    df <- x %>% 
+      tibble::enframe() %>% 
+      purrr::set_names(c("variable", "value"))
+    
+    # Switch variable into a date
+    if (variable_as_date && stringr::str_detect(df$variable[1], "^X")) {
+      df <- df %>% 
+        rename(date = variable) %>% 
+        mutate(date = stringr::str_remove(date, "^X"), 
+               date = lubridate::ymd_hms(date, tz = tz, truncated = 3))
+    }
+    
+  } else {
+    stop("Not implemented.", call. = FALSE)
+    # raster::calc
+  }
+  
+  return(df)
+  
+}
